c318f24fa828fc1edb499b4e590ed2b4f2dc2a7a
reus <reusee@gmail.com>
2021-03-16 11:14:20 +0800

    add wait tree tracing

--- wait_tree.go
+++ wait_tree.go
@@ -2,7 +2,11 @@ package pr
 
 import (
 	"context"
+	"os"
+	"strconv"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/reusee/e4"
 )
@@ -13,7 +17,22 @@ type WaitTree struct {
 	parentDone      func()
 	onCanceledError func()
 	wg              sync.WaitGroup
+
+	sync.Mutex
+	traces map[string]int
 }
+
+var traceWaitTree = func() int {
+	env := os.Getenv("TRACE_WAIT_TREE")
+	if len(env) == 0 {
+		return 0
+	}
+	n, err := strconv.Atoi(env)
+	if err != nil {
+		panic("bad TRACE_WAIT_TREE value")
+	}
+	return n
+}()
 
 func NewWaitTree(
 	parent *WaitTree,
@@ -21,6 +40,7 @@ func NewWaitTree(
 ) *WaitTree {
 	tree := &WaitTree{
 		onCanceledError: onCanceledError,
+		traces:          make(map[string]int),
 	}
 	if parent != nil {
 		tree.parentDone = parent.Add()
@@ -45,16 +65,46 @@ func (t *WaitTree) Add() (done func()) {
 	default:
 	}
 	t.wg.Add(1)
+	var stack string
+	if traceWaitTree > 0 {
+		stack = e4.NewStacktrace()(nil).Error()
+		t.Lock()
+		t.traces[stack]++
+		t.Unlock()
+	}
 	var doneOnce sync.Once
 	return func() {
 		doneOnce.Do(func() {
 			t.wg.Done()
+			if traceWaitTree > 0 {
+				t.Lock()
+				t.traces[stack]--
+				t.Unlock()
+			}
 		})
 	}
 }
 
 func (t *WaitTree) Wait() {
-	t.wg.Wait()
+	if traceWaitTree > 0 {
+		var ok int64
+		time.AfterFunc(time.Second*time.Duration(traceWaitTree), func() {
+			if atomic.CompareAndSwapInt64(&ok, 0, 1) {
+				t.Lock()
+				for stack, n := range t.traces {
+					if n == 0 {
+						continue
+					}
+					pt("WAIT TREE BLOCKING: %s\n", stack)
+				}
+				t.Unlock()
+			}
+		})
+		t.wg.Wait()
+		atomic.StoreInt64(&ok, 1)
+	} else {
+		t.wg.Wait()
+	}
 }
 
 func (t *WaitTree) Done() {
