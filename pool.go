ae143efb46398dcaafddc75050cc3961e4b5788b
reus <reusee@gmail.com>
2022-02-07 16:41:06 +0800

    type-parameterize Pool

--- pool.go
+++ pool.go
@@ -6,36 +6,36 @@ import (
 	"sync/atomic"
 )
 
-type Pool struct {
-	newFunc    func() any
-	pool       []_PoolElem
+type Pool[T any] struct {
+	newFunc    func() T
+	pool       []_PoolElem[T]
 	Callers    [][]byte
 	capacity   int32
 	n          int32
 	LogCallers bool
 }
 
-type _PoolElem struct {
+type _PoolElem[T any] struct {
 	Taken  uint32
 	Refs   int32
-	Value  any
 	Put    func() bool
 	IncRef func()
+	Value  T
 }
 
-func NewPool(
+func NewPool[T any](
 	capacity int32,
-	newFunc func() any,
-) *Pool {
+	newFunc func() T,
+) *Pool[T] {
 
-	pool := &Pool{
+	pool := &Pool[T]{
 		capacity: capacity,
 		newFunc:  newFunc,
 	}
 
 	for i := int32(0); i < capacity; i++ {
 		i := i
-		pool.pool = append(pool.pool, _PoolElem{
+		pool.pool = append(pool.pool, _PoolElem[T]{
 			Value: newFunc(),
 
 			Put: func() bool {
@@ -61,13 +61,13 @@ func NewPool(
 	return pool
 }
 
-func (p *Pool) Get() (value any, put func() bool) {
+func (p *Pool[T]) Get() (value T, put func() bool) {
 	value, put, _ = p.GetRC()
 	return
 }
 
-func (p *Pool) GetRC() (
-	value any,
+func (p *Pool[T]) GetRC() (
+	value T,
 	put func() bool,
 	incRef func(),
 ) {
@@ -99,15 +99,15 @@ func noopPut() bool {
 func noopIncRef() {
 }
 
-func (p *Pool) Getter() (
-	get func() any,
+func (p *Pool[T]) Getter() (
+	get func() T,
 	putAll func(),
 ) {
 
 	var l sync.Mutex
 	curPut := func() {}
 
-	get = func() any {
+	get = func() T {
 		v, put := p.Get()
 		l.Lock()
 		cur := curPut
